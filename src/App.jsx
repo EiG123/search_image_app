b35772767a95a5b273a8424d2300d50e8eeccb79
EiG123 <narinsuwan567@gmail.com>
2024-02-18 17:32:11 +0700

    show image API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
 import { useState } from "react";
 import "./App.css";
+import Picture from "./components/Picture";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [photos, setPhotos] = useState([]);
+  const key="vlT-4aWXAGhkzIHaeqBiuDgl_OTErUwsc0EW5FtYhjM"
+
+  function searchImage(e) {
+    e.preventDefault();
+    if (!word) {
+      alert("กรุณาป้อนชื่อรูปภาพ");
+    } else {
+      //เรียกใช้งาน API
+      fetchImageFromAPI()
+    }
+  }
+
+  async function fetchImageFromAPI(){
+    const url=`https://api.unsplash.com/search/photos?page=1&query=${word}&client_id=${key}&per_page=15`
+    const res = await fetch(url)
+    const data = await res.json()
+    const result = data.results
+    if(result.length==0){
+      alert("ไม่มีข้อมูลรูปภาพ")
+      setWord("")
+    }else{
+      //แสดงข้อมูลรูปภาพ
+      setPhotos(result)
+    }
+  }
   return (
     <>
       <h1>ระบบค้นหารูปภาพด้วย API</h1>
-      <form>
-        <input type="text" placeholder="ป้อนชื่อรูปภาพที่ต้องการค้นหา"/>
+      <form onSubmit={searchImage}>
+        <input
+          type="text"
+          placeholder="ป้อนชื่อรูปภาพที่ต้องการค้นหา"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
         <button type="submit">ค้นหา</button>
       </form>
+      <div className="search-result">
+        {photos.map((data, index)=>{
+          return <Picture {...data} key={index}/>
+        })}
+      </div>
     </>
   );
 }
